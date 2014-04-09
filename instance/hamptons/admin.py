10888d07ec3ba70a3efa8c88239f977c1593149e
Alexander Böhn <fish2000@gmail.com>
2014-04-09 19:41:57 -0400

    Admin pages for Hampton user

--- instance/hamptons/admin.py
+++ instance/hamptons/admin.py
@@ -1,2 +1,11 @@
 
-# django admin
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from hamptons.models import Hamptonian
+
+class HamptonianAdministration(UserAdmin):
+    pass
+
+#admin.site.unregister(User)
+admin.site.register(Hamptonian, HamptonianAdministration)
