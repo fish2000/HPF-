49a13563569fe4452dd0c89531d9731952a1d9d2
Alexander Böhn <fish2000@gmail.com>
2014-04-15 11:34:12 -0400

    Admin interface for Frampton objects

--- instance/hamptons/admin.py
+++ instance/hamptons/admin.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
-from hamptons.models import Hamptonian
+from hamptons.models import Hamptonian, Frampton
 
 class HamptonianAdministration(UserAdmin):
     pass
 
 #admin.site.unregister(User)
 admin.site.register(Hamptonian, HamptonianAdministration)
+admin.site.register(Frampton)
