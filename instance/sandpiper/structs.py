9bf4adc294bcf6c17cbcb3249afbad7ae955b0bb
Alexander Böhn <fish2000@gmail.com>
2014-06-02 15:37:36 -0400

    RedisDict structure's methods have the correct docstrings (copied from those of the dict builtin)

--- instance/sandpiper/structs.py
+++ instance/sandpiper/structs.py
@@ -1,7 +1,36 @@
 
+from __future__ import print_function
+
+VERBOSE = False
+
+dict_dict = dict.__dict__
+dict_keys = dict_dict.keys()
 
 class RedisDict(object):
-    """ Dict wrapper for a Redis set"""
+    """ Dict wrapper for a Redis set """
+    
+    class __metaclass__(type):
+        def __new__(cls, name, bases, attrs):
+            """ Get docstrings for our duckpunched dict methods,
+                from the Python builtin dict base class """
+            
+            for attr_name in attrs.keys():
+                if attr_name in dict_keys:
+                    if hasattr(dict_dict[attr_name], '__doc__'):
+                        # If you try to copy docstrings for an aliased dict method,
+                        # either an AttributeError or (more likely) a TypeError
+                        # will stop the show -- hence all of this stuff:
+                        try:
+                            attrs[attr_name].__doc__ = dict_dict[attr_name].__doc__
+                        except (TypeError, AttributeError), err:
+                            if VERBOSE:
+                                print("%s with attribute %s: %s" % (type(err).__name__, attr_name, err))
+                        else:
+                            if VERBOSE:
+                                print("Copied docstring for attribute: %s" % attr_name)
+            
+            return type(name, bases, attrs)
+    
     def __init__(self, username, redis):
         self.u = username
         self.r = redis
@@ -27,6 +56,7 @@ class RedisDict(object):
         self.r.hdel(self.u, key)
     
     def setdefault(self, key, default=None):
+        # We cannot (yet) set dict-default methods
         raise NotImplementedError("Fuck your defaults")
     
     def keys(self):
@@ -37,8 +67,10 @@ class RedisDict(object):
     
     @property
     def dict(self):
+        """ Return a vanilla Python dict populated from the RedisDict """
         return self.r.hgetall(self.u)
     
+    # Methods aliased from the dict builtin
     has_key = property(lambda self: self.dict.has_key)
     get = property(lambda self: self.dict.get)
     items = property(lambda self: self.dict.items)
@@ -48,8 +80,8 @@ class RedisDict(object):
     viewitems = property(lambda self: self.dict.viewitems)
     viewkeys = property(lambda self: self.dict.viewkeys)
     viewvalues = property(lambda self: self.dict.viewvalues)
-    copy = property(lambda self: self.dict.copy) # not quite right
-    fromkeys = property(lambda self: self.dict.fromkeys)
+    #copy = property(lambda self: self.dict.copy) # not quite right
+    fromkeys = property(lambda self: self.dict.fromkeys) # not quite right either
     __eq__ = property(lambda self: self.dict.__eq__)
     __ne__ = property(lambda self: self.dict.__ne__)
     __cmp__ = property(lambda self: self.dict.__cmp__)
@@ -60,10 +92,14 @@ class RedisDict(object):
     __repr__ = property(lambda self: self.dict.__repr__)
     __str__ = property(lambda self: self.dict.__str__)
     
+    # Unimplemented dict methods
     __lt__ = property(lambda self: NotImplemented)
     __gt__ = property(lambda self: NotImplemented)
     __le__ = property(lambda self: NotImplemented)
     __ge__ = property(lambda self: NotImplemented)
+    
+    def copy(self):
+        return RedisDict(self.u, self.r)
     
     def pop(self, key, default=None):
         if key not in self:
@@ -85,6 +121,8 @@ class RedisDict(object):
         self.r.hdel(*self.keys())
     
     def update(self, E, **F):
+        # This is copied right out of the Python docs
+        # for the dict builtin's methods
         if hasattr(E, 'keys'):
             for k in E:
                 self[k] = E[k]
